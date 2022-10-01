a56f9a1c7599b624788c8861322678c8bce1af15
Sebastian Grunow <sebastian@prodigy.solutions>
2022-10-01 20:09:38 +0200

    Fix flood threshold and Change touch sensor pin.

--- hydroponics_watering/constants.h
+++ hydroponics_watering/constants.h
@@ -1,10 +1,9 @@
 #ifndef CONSTANTS_H
 #define CONSTANTS_H
 
-#define FLOOD_THRESHOLD 65.0
-#define DRAIN_THRESHOLD 60.0
+#define FLOOD_THRESHOLD 30.0
 
-static const uint8_t PIN_SENSOR_FLOOD_COMPLETE = 32;
+static const uint8_t PIN_SENSOR_FLOOD_COMPLETE = 33;
 
 static const uint8_t PIN_PUMP = 17;
 static const uint8_t PIN_VALVE = 16;
